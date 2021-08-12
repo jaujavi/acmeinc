57cd52f01daa46ca73b5bb3c83ccedd369870018
Javier Torre Antunez <jaujavi@gmail.com>
2021-08-12 19:17:04 +0200

    testing string

--- acmeinc.py
+++ acmeinc.py
@@ -3,14 +3,19 @@ import csv
 def print_products (file_name):
     try:
         with open(file_name, 'r') as file:
+
             file_csv = csv.DictReader(file) #default delimiter is ","
             lines = 0
+
             for line in file_csv:
                 print('products ->',line["products"])
                 lines += 1
+
+                products = line["products"].split()
+                for product in products:
+                    print("\tproduct -> ", product)
     except IOError:
         print('ERROR: File', file_name,'not found.')
-
 
 def main():
 
