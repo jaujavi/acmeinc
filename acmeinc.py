90482e38a1c3812d495b7198b67ee70f104aea39
Javier Torre Antunez <jaujavi@gmail.com>
2021-08-14 15:04:13 +0200

    prints cleaned

--- acmeinc.py
+++ acmeinc.py
@@ -14,7 +14,6 @@ def print_csv_header(file_name,header):
 def print_csv_row(file_name,row):
     try:
         with open(file_name, 'a+', newline='') as file_a:
-            #file_csv_a = csv.DictWriter(file_a, fieldnames=header)
             file_csv_a = csv.writer(file_a) #default delimiter is ","
             file_csv_a.writerow(row)
     except Exception as e:
@@ -36,10 +35,7 @@ def get_order_price(order, file_products):
     total = 0
     products = order['products'].split()
     for product_id in products:
-        #product_price = get_product_price(product_id, file_products)
-        #print("\tproduct:", product_id,'\tprice:',product_price)
         total += get_product_price(product_id, file_products)
-    print('Total_order_price ->', total,'\n')
     return float(total)
 
 
@@ -49,9 +45,6 @@ def create_order_prices(file_orders, file_products, file_order_prices, file_orde
         with open(file_orders, 'r') as file_r:
             file_orders_csv = csv.DictReader(file_r) #default delimiter is ","
             for order in file_orders_csv:
-                print('Products for order:',order['id'],' ->',order['products'])
-                #order_price = get_order_price(order, file_products)
-                #order_price = {'id': order["id"], 'euros': total_order_price}
                 order_price_row = (order['id'], get_order_price(order, file_products))
                 print_csv_row(file_order_prices, order_price_row)
     except IOError:
@@ -77,10 +70,8 @@ def create_products_customers(file_orders, file_products, file_products_customer
         with open(file_products, 'r') as file_r:
             file_products_csv = csv.DictReader(file_r) #default delimiter is ","
             for product in file_products_csv:
-                print('Checking orders for product:',product['id'])
                 customers_ids = get_product_customers(product['id'], file_orders)
                 customers_ids_spaced = ' '.join(customers_ids)
-                print('Customers that buy the product id:',product['id'],'->',customers_ids_spaced)
                 product_customers_row = (product['id'], customers_ids_spaced)
                 print_csv_row(file_products_customers, product_customers_row)
     except IOError:
